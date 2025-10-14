9ebee08f6d3f23bd6352e1eccaa370b8b6b176ff
Sombre18 <clairelawand@gmail.com>
2025-10-14 23:20:36 +0200

    Attempt at Week-5

--- Week-5/Exercises/blindspot.py
+++ Week-5/Exercises/blindspot.py
@@ -1,11 +1,13 @@
 from expyriment import design, control, stimuli
-from expyriment.misc.constants import C_WHITE, C_BLACK
-from expyriment.misc.constants import K_ALL_DIGITS, K_DOWN, K_UP, K_LEFT, K_RIGHT
+from expyriment.misc.constants import C_WHITE, C_BLACK, K_SPACE, K_1, K_2, K_DOWN, K_UP, K_LEFT, K_RIGHT
 
 """ Global settings """
 exp = design.Experiment(name="Blindspot", background_colour=C_WHITE, foreground_colour=C_BLACK)
+exp.add_data_variable_names(["key", "radius", "position"])
 control.set_develop_mode()
 control.initialize(exp)
+
+keys = [K_1, K_2, K_UP, K_DOWN, K_RIGHT, K_LEFT, K_SPACE]
 
 """ Stimuli """
 def make_circle(r, pos=(0,0)):
@@ -18,24 +20,39 @@ def run_trial():
     fixation = stimuli.FixCross(size=(150, 150), line_width=10, position=[300, 0])
     fixation.preload()
     radius = 75
-    #position = (0,0)
+    position= [0,0]
+
+    circle = make_circle(radius, position)
+    
     while True:
-        key, _ = exp.keyboard.wait(keys = K_ALL_DIGITS[1])
-        if key: 1
-        radius -= 1
-        if key: 2
-        radius += 1
-    return radius
+        fixation.present(clear=True, update=False)
+        circle.present(clear=False, update=True)
+        
+        key, _ = exp.keyboard.wait(keys=keys)
+        if key == K_SPACE:
+            break
+        if key == K_1:
+            radius = max(1, radius - 5)
+            circle = make_circle(radius, position)
+        if key == K_2:
+            radius = max(1, radius + 5)
+            circle = make_circle(radius, position)
+        if key == K_DOWN:
+            position = (0, -10)
+            circle = make_circle(radius, position)
+        if key == K_UP:
+            position = (0, +10)
+            circle = make_circle(radius, position)
+        if key == K_LEFT:
+            position = (-10, 0)
+            circle = make_circle(radius, position)
+        if key == K_RIGHT:
+            position = (+10, 0)
+            circle = make_circle(radius, position)
+    exp.data.add([key, radius, position])
+        
 
-            
 
-    #position = exp.keyboard.wait(keys = [K_DOWN, K_UP, K_LEFT, K_RIGHT])
-    circle = make_circle(radius)
-
-    fixation.present(True, False)
-    circle.present(False, True)
-
-    exp.keyboard.wait()
 
 control.start(subject_id=1)
 
