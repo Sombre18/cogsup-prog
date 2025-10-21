9195014d5aa48fef356e5cb24e9ef32f8641f1fb
Sombre18 <clairelawand@gmail.com>
2025-10-21 11:47:33 +0200

    Finished Week-6

--- Week-6/Exercises/stroop_balanced.py
+++ Week-6/Exercises/stroop_balanced.py
@@ -1,23 +1,28 @@
 from expyriment import design, control, stimuli
-from expyriment.misc.constants import C_WHITE, C_BLACK, K_j, K_f
+from expyriment.misc.constants import C_WHITE, C_BLACK, K_f, K_g, K_h, K_j
 import random
 import itertools 
 
 """ Constants """
-KEYS = [K_j, K_f]
-TRIAL_TYPES = ["match", "mismatch"]
+
 COLORS = ["red", "blue", "green", "orange"]
+COLOR_KEYS = {"red": K_f, "blue": K_g, "green": K_h, "orange": K_j}
+KEY_NAMES = {"red": "F", "blue": "G", "green": "H", "orange": "J"}
 
 
-N_BLOCKS = 2
+N_BLOCKS = 8
 N_TRIALS_IN_BLOCK = 16
 
 INSTR_START = """
-In this task, you have to indicate whether the meaning of a word and the color of its font match.
-Press J if they do, F if they don't.\n
+In this task, you have to indicate the colour of each word.
+Press:
+  F = RED
+  G = BLUE
+  H = GREEN
+  J = ORANGE\n
 Press SPACE to continue.
 """
-INSTR_MID = """You have finished half of the experiment, well done! Your task will be the same.\nTake a break then press SPACE to move on to the second half."""
+INSTR_MID = """End of block\nTake a break then press SPACE to move on to the next one."""
 INSTR_END = """Well done!\nPress SPACE to quit the experiment."""
 
 FEEDBACK_CORRECT = """Correct!"""
@@ -64,27 +69,32 @@ feedback_correct = stimuli.TextLine(FEEDBACK_CORRECT)
 feedback_incorrect = stimuli.TextLine(FEEDBACK_INCORRECT)
 load([feedback_correct, feedback_incorrect])
 
+all_combinations = list(itertools.product(COLORS, COLORS)) 
+blocks = []
+for _ in range(N_BLOCKS):
+    block_trials = all_combinations.copy()
+    random.shuffle(block_trials)
+    blocks.append(block_trials)
+
 
 """ Experiment """
-def run_trial(block_id, trial_id, trial_type, word, color):
+def run_trial(block_id, trial_id, word, color):
     stim = stims[word][color]
     present_for(fixation, t=500)
     stim.present()
-    key, rt = exp.keyboard.wait(KEYS)
-    correct = key == K_j if trial_type == "match" else key == K_f
-    exp.data.add([block_id, trial_id, trial_type, word, color, rt, correct])
+    key, rt = exp.keyboard.wait(COLOR_KEYS.values())
+    correct = key == COLOR_KEYS[color]
+    exp.data.add([block_id, trial_id, word, color, rt, correct])
     feedback = feedback_correct if correct else feedback_incorrect
     present_for(feedback, t=1000)
 
 control.start(subject_id=1)
 
 present_instructions(INSTR_START)
-for block_id in range(1, N_BLOCKS + 1):
-    for trial_id in range(1, N_TRIALS_IN_BLOCK + 1):
-        trial_type = random.choice(TRIAL_TYPES)
-        word = random.choice(COLORS)
-        color = word if trial_type == "match" else random.choice([c for c in COLORS if c != word])
-        run_trial(block_id, trial_id, trial_type, word, color)
+trial_index = 0
+for block_id, block_trials in enumerate(blocks, start=1):
+    for trial_id, (word, color) in enumerate(block_trials, start=1):
+        run_trial(block_id, trial_id, word, color)
     if block_id != N_BLOCKS:
         present_instructions(INSTR_MID)
 present_instructions(INSTR_END)
